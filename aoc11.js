6e04943f6bf67ed9cf488d493b72dcb2bdce8226
James Wordie <james.wordie@yahoo.com>
2021-12-11 11:51:21 +0000

    complete 2 with comments

--- aoc11.js
+++ aoc11.js
@@ -27,51 +27,50 @@ const inputArr = input.split(/\n/).map((row) => row.split("").map(Number));
 
 // Part 1
 
-const checkSurround = (input, row, column, flashed = new Set()) => {
-  if (row < 0 || row >= input.length) return;
-  if (column < 0 || column >= input[0].length) return;
+const checkSurround = (grid, row, column, flashed = new Set()) => {
+  if (row < 0 || row >= grid.length) return;
+  if (column < 0 || column >= grid[0].length) return;
   if (flashed.has(`${row}${column}`)) return;
 
-  const value = input[row][column];
-  const newValue = value + 1;
-  input[row][column] = newValue;
-  if (newValue <= 9) {
+  const value = grid[row][column];
+  const newVal = value + 1;
+  grid[row][column] = newVal;
+  if (newVal <= 9) {
     return;
   }
 
   flashed.add(`${row}${column}`);
-  input[row][column] = 0;
+  grid[row][column] = 0;
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
       if (i === 0 && j === 0) {
         continue;
       }
-      checkSurround(input, row + i, column + j, flashed);
+      checkSurround(grid, row + i, column + j, flashed);
     }
   }
 };
 
-const evalArr = (input) => {
+const evalArr = (grid) => {
   const flashed = new Set();
-  for (let row = 0; row < input.length; row++) {
-    const rowArr = input[row];
-    for (let col = 0; col < rowArr.length; col++) {
-      checkSurround(input, row, col, flashed);
+  for (let row = 0; row < grid.length; row++) {
+    const rowLength = grid[row].length;
+    for (let col = 0; col < rowLength; col++) {
+      checkSurround(grid, row, col, flashed);
     }
   }
   return flashed.size;
 };
 
 const part1 = (input) => {
-  let totalFlashed = 0;
+  let finalCount = 0;
   for (let i = 0; i < 100; i++) {
-    totalFlashed += evalArr(input);
+    finalCount += evalArr(input);
   }
-  return totalFlashed;
+  return finalCount;
 };
 
-console.log("Part1");
-console.log(part1(inputArr));
+console.log(part1(inputArr), "Part 1");
 
 // Part 2
 
@@ -86,102 +85,5 @@ const part2 = (input) => {
   }
   return count + 100;
 };
-console.log("Part2");
-console.log(part2(inputArr));
 
-// const DIRECTIONS = [
-//   [-1, -1],
-//   [-1, 0],
-//   [-1, 1],
-//   [0, -1],
-//   [0, 1],
-//   [1, -1],
-//   [1, 0],
-//   [1, 1],
-// ];
-
-// export function part1(input: string): number {
-//   let flashedCount = 0;
-//   const octopi = input.split("\n").map((line) =>
-//     line.split("").map((char) => +char)
-//   );
-//   for (let step = 0; step < 100; step++) {
-//     const flashed: [number, number][] = [];
-//     for (let y = 0; y < octopi.length; y++) {
-//       for (let x = 0; x < octopi[0].length; x++) {
-//         if (++octopi[y][x] === 10) {
-//           flashed.push([y, x]);
-//         }
-//       }
-//     }
-//     while (flashed.length) {
-//       const [y0, x0] = flashed.pop()!;
-//       flashedCount++;
-//       for (const [yd, xd] of DIRECTIONS) {
-//         const y1 = y0 + yd;
-//         if (y1 < 0 || octopi.length <= y1) {
-//           continue;
-//         }
-//         const x1 = x0 + xd;
-//         if (x1 < 0 || octopi[0].length <= x1) {
-//           continue;
-//         }
-//         if (++octopi[y1][x1] === 10) {
-//           flashed.push([y1, x1]);
-//         }
-//       }
-//     }
-//     for (let y = 0; y < octopi.length; y++) {
-//       for (let x = 0; x < octopi[0].length; x++) {
-//         if (octopi[y][x] >= 10) {
-//           octopi[y][x] = 0;
-//         }
-//       }
-//     }
-//   }
-//   return flashedCount;
-// }
-
-// function part2(octopi) {
-//   let step = 0,
-//     flashedCount;
-//   do {
-//     step++;
-//     flashedCount = 0;
-//     const flashed = [];
-//     for (let y = 0; y < octopi.length; y++) {
-//       for (let x = 0; x < octopi[0].length; x++) {
-//         if (++octopi[y][x] === 10) {
-//           flashed.push([y, x]);
-//         }
-//       }
-//     }
-//     while (flashed.length) {
-//       const [y0, x0] = flashed.pop();
-//       flashedCount++;
-//       for (const [yd, xd] of DIRECTIONS) {
-//         const y1 = y0 + yd;
-//         if (y1 < 0 || octopi.length <= y1) {
-//           continue;
-//         }
-//         const x1 = x0 + xd;
-//         if (x1 < 0 || octopi[0].length <= x1) {
-//           continue;
-//         }
-//         if (++octopi[y1][x1] === 10) {
-//           flashed.push([y1, x1]);
-//         }
-//       }
-//     }
-//     for (let y = 0; y < octopi.length; y++) {
-//       for (let x = 0; x < octopi[0].length; x++) {
-//         if (octopi[y][x] >= 10) {
-//           octopi[y][x] = 0;
-//         }
-//       }
-//     }
-//   } while (flashedCount !== octopi.length * octopi[0].length);
-//   return step;
-// }
-
-// console.log(part2(inputArr));
+console.log(part2(inputArr), "Part 2");
