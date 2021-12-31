ffedaba726adb22d5bb8cc326a97da6604e1c940
James Wordie <james.wordie@yahoo.com>
2021-12-31 21:57:06 +0000

    Day 24 reddit solution for learning - not submitted

--- Day 24/aoc24.js
+++ Day 24/aoc24.js
@@ -1,12 +1,40 @@
 import { input } from "./input.js";
 
-let [w, x, y, z] = [0, 0, 0, 0];
+// let [w, x, y, z] = [0, 0, 0, 0];
 
-const sortCommands = input.split(/\n/).map((row) => {
-  let tmp = row.match(/(\w{3})\s{1}(\d{1})\s(\d?)/g);
-  console.log(tmp);
-});
+// const sortCommands = input.split(/\n/).map((row) => {
+//   let tmp = row.match(/(\w{3})\s{1}(\d{1})\s(\d?)/g);
+//   console.log(tmp);
+// });
 
-console.log(sortCommands);
+// console.log(sortCommands);
 
-const filterInput = (input) => {};
+// const filterInput = (input) => {};
+
+////////////// SOLUTION FROM REDDIT - NOT SUBMITTED //////////////////
+
+function solve(input, part) {
+  const lines = input.split("\n");
+
+  const terms = [];
+  for (let i = 0; i < lines.length; i += 18) {
+    terms.push([4, 5, 15].map((j) => +lines[i + j].split(" ")[2]));
+  }
+
+  const prevs = [];
+  const digits = [];
+  for (const [i, [a, b, c]] of Object.entries(terms)) {
+    if (a === 1) {
+      prevs.push([i, c]);
+    } else {
+      const [prevI, prevC] = prevs.pop();
+      const complement = prevC + b;
+      digits[prevI] =
+        part === 1 ? Math.min(9, 9 - complement) : Math.max(1, 1 - complement);
+      digits[i] = digits[prevI] + complement;
+    }
+  }
+  return digits.join("");
+}
+console.log(solve(input, 1), "Part 1");
+console.log(solve(input, 2), "Part 2");
