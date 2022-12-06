741d4708125fbadc544ce451882cf8e7daf362c4
James Wordie <james.wordie@yahoo.com>
2022-12-06 16:50:22 +0000

    tmp update to day 6

--- 2022/day-6/day_6.py
+++ 2022/day-6/day_6.py
@@ -1,24 +1,25 @@
 sample = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 
 with open('day-6/input.txt', encoding='utf-8') as file:
-    data = [i for i in file.read().strip().split("\n")]
+    data = file.read().strip()
 
 split = []
 for i in data:
   split += i
 
-def check_unique(string):
-  for i in range(len(string)):
-    for j in range(i + 1,len(string)):
-      if(string[i] == string[j]):
-        return False
-  return True
+# def check_unique(string):
+  # for i in range(len(string)):
+  #   for j in range(i + 1,len(string)):
+  #     if(string[i] == string[j]):
+  #       return False
+  # return True
 
 def solve(split_arr, skip):
   start = 0
   end = 0
   while start < len(split_arr):
-    if check_unique(split_arr[start:start + skip]):
+    if len(set(split_arr[start:start + skip])) == skip:
+    # if check_unique(split_arr[start:start + skip]):
       end = start + skip
       break
     else:
@@ -28,3 +29,10 @@ def solve(split_arr, skip):
 
 print('Part 1', solve(split, 4))
 print('Part 2', solve(split, 14))
+
+# Shortened solution from someone at Le Wagon AOC
+# part_1 = next(i + 4 for i, _ in enumerate(split) if len(set(split[i:i + 4])) == 4)
+# part_2 = next(i + 14 for i, _ in enumerate(split) if len(set(split[i:i + 14])) == 14)
+
+# print(part_1)
+# print(part_2)
