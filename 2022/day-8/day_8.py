8aae6f7dcb4da060a9e5392db785047bf530d0c2
James Wordie <james.wordie@yahoo.com>
2022-12-08 20:42:34 +0000

    Completed day 8 part 2

--- 2022/day-8/day_8.py
+++ 2022/day-8/day_8.py
@@ -1,4 +1,4 @@
-with open('day-8/sample.txt', encoding='utf-8') as file:
+with open('day-8/input.txt', encoding='utf-8') as file:
     data = [j.split() for j in [i for i in file.read().strip().split("\n")]]
 # print(data)
 
@@ -9,28 +9,28 @@ for row in data:
 # print(grid)
 
 # Check along the current row, from pos to left edge
-def leftCheck(row, col):
+def CheckToLeft(row, col):
     for i in range(0, col):
         if grid[row][i] >= grid[row][col]:
             return False
     return True
 
 # Check along the current row, from pos to right edge
-def rightCheck(row, col):
+def checkToRight(row, col):
     for i in range(col+1, len(grid[0])):
         if grid[row][i] >= grid[row][col]:
             return False
     return True
 
 # Check along the current col, from pos to top edge
-def upCheck(row, col):
+def checkToTop(row, col):
     for i in range(0,row):
         if grid[i][col] >= grid[row][col]:
             return False
     return True
 
 # Check along the current row, from pos to bottom edge
-def downCheck(row, col):
+def checkToBottom(row, col):
     for i in range(row+1,len(grid)):
         if grid[i][col] >= grid[row][col]:
             return False
@@ -40,7 +40,50 @@ def downCheck(row, col):
 visible = 2*(len(grid)+len(grid[0])-2)
 for row in range(1, len(grid)-1):
   for col in range(1, len(grid[0])-1):
-    if leftCheck(row, col) or rightCheck(row, col) or upCheck(row, col) or downCheck(row, col):
+    if CheckToLeft(row, col) or checkToRight(row, col) or checkToTop(row, col) or checkToBottom(row, col):
       visible += 1
 
 print('Part 1', visible)
+
+# Part 2
+def countToLeft(row, col):
+  total = 0
+  for i in range(col-1, -1, -1):
+    if grid[row][i] < grid[row][col]:
+      total += 1
+    else:
+      return total+1
+  return total
+
+def countToRight(row, col):
+  total = 0
+  for i in range(col+1, len(grid[0])):
+    if grid[row][i] < grid[row][col]:
+      total += 1
+    else:
+      return total+1
+  return total
+
+def countToTop(row, col):
+  total = 0
+  for i in range(row-1, -1, -1):
+    if grid[i][col] < grid[row][col]:
+      total += 1
+    else:
+      return total+1
+  return total
+
+def countToBottom(row, col):
+  total = 0
+  for i in range(row+1,len(grid)):
+    if grid[i][col] < grid[row][col]:
+      total += 1
+    else:
+      return total+1
+  return total
+
+maximum = 0
+for row in range(1, len(grid)-1):
+    for col in range(1, len(grid[0])-1):
+        maximum = max(maximum, countToLeft(row, col) * countToRight(row, col) * countToTop(row, col) * countToBottom(row, col))
+print('Part 2', maximum)
