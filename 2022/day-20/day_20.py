b8a09f84951bd7a6c7e7f4375da628c261c15976
James Wordie <james.wordie@yahoo.com>
2022-12-21 07:44:17 +0000

    Completed day 20 and part1 of day 21

--- 2022/day-20/day_20.py
+++ 2022/day-20/day_20.py
@@ -1,3 +1,58 @@
-with open('day-20/sample.txt', encoding='utf-8') as file:
-    data = [i for i in file.read().strip().split("\n")]
-# print(data)
+
+def get_data(key):
+    with open('day-20/input.txt', encoding='utf-8') as file:
+        numbers = [(ind, int(val) * key) for ind, val in enumerate(file.read().strip().split("\n"))]
+        original_nums = [n[1] for n in numbers]
+    return numbers, original_nums
+
+# print(numbers)
+# print(original_nums)
+
+def mix(nums, original):
+    for ind, num in enumerate(original):
+        number = num
+        find = (ind, num)
+
+        # Find index of num
+        index = nums.index(find)
+
+        # Generate left and right lists
+        left = nums[:index]
+        right = nums[index + 1:]
+        temp = left + right
+
+        # Insert new pos
+        new_pos = (index + number) % len(temp)
+        if new_pos == 0:
+            temp.append(find)
+        else:
+            temp.insert(new_pos, find)
+
+        # Update nums
+        nums = temp
+    return nums
+
+def get_coords(nums):
+    counter = 0
+    indices = [1000, 2000, 3000]
+
+    # Find the zero index
+    for ind, val in enumerate(nums):
+        if val[1] == 0:
+            break
+
+    # Get coords
+    for i in indices:
+        counter += nums[(ind + i) % len(nums)][1]
+    return counter
+
+nums, orig_nums = get_data(1)
+mixed = mix(nums, orig_nums)
+print('Part 1:', get_coords(mixed))
+
+nums, orig_nums = get_data(811589153)
+for i in range(10):
+    shuffled = mix(nums, orig_nums)
+    nums = shuffled
+
+print('Part 2:', get_coords(nums))
--- 2022/day-21/day_21.py
+++ 2022/day-21/day_21.py
@@ -1,3 +1,50 @@
-with open('day-21/sample.txt', encoding='utf-8') as file:
+import re
+from math import floor
+
+with open('day-21/input.txt', encoding='utf-8') as file:
     data = [i for i in file.read().strip().split("\n")]
 # print(data)
+
+monkeys = {}
+for line in data:
+    monkey_id = re.findall('^\w+:', line)[0].replace(':', '')
+    operations = re.findall('\w+ [+*-/] \w+', line)
+    if len(operations) > 0:
+        monkey1, operator, monkey2 = operations[0].split(' ')
+        monkeys[monkey_id] = {
+            'monkey1': monkey1,
+            'monkey2': monkey2,
+            'operator': operator,
+            'number': None
+        }
+    number = re.findall('\d+', line)
+    if len(number) > 0:
+        monkeys[monkey_id] = {
+        'monkey1': None,
+        'monkey2': None,
+        'operator': None,
+        'number': int(number[0])
+    }
+# print(monkeys)
+
+def monkey_math(curr, mnk_list):
+    num = mnk_list[curr]['number']
+    mon1 = mnk_list[curr]['monkey1']
+    mon2 = mnk_list[curr]['monkey2']
+    oper = mnk_list[curr]['operator']
+
+    # If a number or float return the monkey
+    if isinstance(num, int) or isinstance(num, float):
+        return mnk_list[curr]['number']
+
+    if oper == '+':
+        return monkey_math(mon1, mnk_list) + monkey_math(mon2, mnk_list)
+    elif oper == '-':
+        return monkey_math(mon1, mnk_list) - monkey_math(mon2, mnk_list)
+    elif oper == '/':
+        return monkey_math(mon1, mnk_list) / monkey_math(mon2, mnk_list)
+    elif oper == '*':
+        return monkey_math(mon1, mnk_list) * monkey_math(mon2, mnk_list)
+
+res1 = monkey_math('root', monkeys)
+print('Part 1:', floor(res1))
