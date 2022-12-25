10b2915fd7a503e73a96456262fac9c54aafab39
James Wordie <james.wordie@yahoo.com>
2022-12-25 08:37:07 +0000

    Solved part 1 day 25, not enough stars for part 2

--- 2022/day-25/day_25.py
+++ 2022/day-25/day_25.py
@@ -1,3 +1,38 @@
-with open('day-25/sample.txt', encoding='utf-8') as file:
+with open('day-25/input.txt', encoding='utf-8') as file:
     data = [i for i in file.read().strip().split("\n")]
 # print(data)
+
+digits = {
+    '2': 2,
+    '1': 1,
+    '0': 0,
+    '-': -1,
+    '=': -2
+}
+
+transform = {v: k for k, v in digits.items()}
+
+# Convert the snafu string into an integer
+def snafu_2_int(line: str):
+    current = 0
+    for char in line:
+        current *= 5
+        current += digits[char]
+    return current
+
+def int_2_snafu(val: int):
+    if val == 0:
+        return ''
+
+    remainder = (val + 2) % 5 - 2
+    new_val = (val + 2) // 5
+
+    return int_2_snafu(new_val) + transform[remainder]
+
+# Solve for part 1
+def part1(snafu_list: list):
+    total_int = sum(snafu_2_int(snafu_num) for snafu_num in snafu_list)
+
+    return int_2_snafu(total_int)
+
+print('Part 1:', part1(data))
